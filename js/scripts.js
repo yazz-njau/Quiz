ad166a860cd0252010b61cfd15795d0d0ff3b037
Robert njau <48471325+yazz-njau@users.noreply.github.com>
2019-04-01 13:48:05 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
  $(document).ready(function() {
    $("#submit").click(function(event) {
 
-    var questOne = $  ("input.radio[name=question1]:checked").val();
-    var questTwo = $  ("input.radio[name=question2]:checked").val();
-    var questThree = $ ("input.radio[name=question3]:checked").val();
+    var questOne = $  ("input.radio[name=question1]:checked").val(20);
+    var questTwo = $  ("input.radio[name=question2]:checked").val(20);
+    var questThree = $ ("input.radio[name=question3]:checked").val(20);
 
     var yourScore = parseInt(questOne) + parseInt(questTwo) + parseInt(questThree);
     $("#yourScore").text("your score is:" + yourScore + "%");
